b30b87d20650d6133760b09d52e3b2b4640b5df1
Brandon Kreisel <kreiselb@gmail.com>
2018-04-24 18:58:58 -0400

    Simplify Logging Module

--- logger.py
+++ logger.py
@@ -1,36 +1,24 @@
 """ Console and File Logging Functions """
-########## STDLIB IMPORTS ##########
 import os
 import sys
 import logging
 
-from datetime import datetime
-
-########## CUSTOM IMPORTS ##########
 import config
 
-########## FUNCTIONS ##########
-class LogFormatter(logging.Formatter):
-    """ Custom Log Formatting """
-    def format(self, record):
-        time = datetime.fromtimestamp(record.created).strftime("%Y-%m-%d %H:%M:%S")
-        header = "{} [{:s}]::{:s}".format(time, record.module, record.funcName)
-        return "{:60s} - {}".format(header, record.getMessage())
-
-def setupLogger():
-    """ Configure and Initiate Logging """
-
+def setup():
+    """ Configure the Second30 Logging Module """
     if os.path.exists(config.LOGFILE):
         os.remove(config.LOGFILE)
 
+    handlers = [
+        logging.FileHandler(filename=config.LOGFILE),
+        logging.StreamHandler(stream=sys.stdout)
+    ]
 
-    logging.basicConfig(filename=config.LOGFILE, level=logging.INFO)
-
-    console = logging.getLogger(name="console")
-    console_handler = logging.StreamHandler(stream=sys.stdout)
-    console_handler.setFormatter(LogFormatter())
-    console.addHandler(console_handler)
-
-def getConsole():
-    """ Get a Console Object for Logging Messages"""
-    return logging.getLogger(name="console")
+    logging.basicConfig(
+        level=logging.INFO,
+        format='{asctime} {module:>10s}.{funcName:15s} :: {message}',
+        style='{',
+        datefmt='%m/%d/%Y %I:%M:%S %p',
+        handlers=handlers
+    )
