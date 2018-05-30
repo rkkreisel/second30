ab8041c9d9a88e49caa8540e4a993d11cd139820
Brandon Kreisel <kreiselb@gmail.com>
2018-05-29 21:51:10 -0400

    Order and Spread Logic Fix

--- orders.py
+++ orders.py
@@ -6,12 +6,14 @@ import constants
 def buildOrders(ib, action, quantity, price, stop):
 
     parentId = ib.client.getReqId()
-    
+
+    entryPrice = price + config.ENTRY_SPREAD
+
     #Entry Order
     entryOrder = Order(
         action = action,
         orderType = "STP",
-        auxPrice = price,
+        auxPrice = entryPrice,
         lmtPrice = 0,
         faProfile = config.ALLOCATION_PROFILE,
         totalQuantity = quantity,
@@ -21,12 +23,12 @@ def buildOrders(ib, action, quantity, price, stop):
 
     if action == "BUY":
         bracketAction = "SELL"
-        profitPrice = price + config.PROFIT_SPREAD
-        lossPrice = price - config.PROFIT_SPREAD
+        profitPrice = entryPrice + config.PROFIT_SPREAD
+        lossPrice = entryPrice - stop
     else:
         bracketAction = "BUY"
-        profitPrice = price - config.PROFIT_SPREAD
-        lossPrice = price + config.PROFIT_SPREAD
+        profitPrice = entryPrice - config.PROFIT_SPREAD
+        lossPrice = entryPrice + stop
 
     #Profit Order
     profitOrder = Order(
